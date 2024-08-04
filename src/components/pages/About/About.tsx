a8c2799a37df18d71204f62ecdcc7ff943687fbc
DDstar1 <abhuluimendestiny@gmail.com>
2024-08-04 01:57:23 +0100

    'dddddd'

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -26,9 +26,9 @@ function About() {
         American, and Wired. In 2018 I was recognized in Forbes 30 Under 30 –
         Science.
       </p>
-      <AuroraBackground>
-        <Timeline />
-      </AuroraBackground>
+      {/* <AuroraBackground> */}
+      <Timeline />
+      {/* </AuroraBackground> */}
       <div className="flex justify-center items-start gap-20">
         <Interests />
         <Publications />
