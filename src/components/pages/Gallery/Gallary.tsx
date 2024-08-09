bf03b25cc1b73b6897171451ca761718ceebf4ec
DDstar1 <abhuluimendestiny@gmail.com>
2024-08-09 20:14:55 +0100

    vsd

--- src/components/pages/Gallery/Gallary.tsx
+++ src/components/pages/Gallery/Gallary.tsx
@@ -3,20 +3,21 @@ import ImageGallery from "@/components/ui/ImageGallary";
 import { Button } from "@/components/ui/MovingBorder";
 
 const imageList = [
+  "/images/akpos_rbot.jpeg",
+  "/images/akpos_14.jpeg",
   "/images/akpos_1.jpeg",
-  "/images/akpos_2.jpeg",
-  "/images/akpos_3.jpeg",
-  "/images/akpos_4.jpeg",
   "/images/akpos_5.jpeg",
+  "/images/akpos_2.jpeg",
+  "/images/akpos_12.jpeg",
+  "/images/akpos_9.jpeg",
+  "/images/akpos_4.jpeg",
+  "/images/akpos_10.jpeg",
   "/images/akpos_6.jpeg",
   "/images/akpos_7.jpeg",
   "/images/akpos_8.jpeg",
-  "/images/akpos_9.jpeg",
-  "/images/akpos_10.jpeg",
+  "/images/akpos_3.jpeg",
   "/images/akpos_11.jpeg",
-  "/images/akpos_12.jpeg",
   "/images/akpos_13.jpeg",
-  "/images/akpos_14.jpeg",
 ];
 
 const Gallery = () => {
